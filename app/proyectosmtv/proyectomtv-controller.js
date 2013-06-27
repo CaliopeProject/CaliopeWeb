6113139acf91a7627da5ceb06987c3e23c0cf275
ndochoa <ndaniel8a@gmail.com>
2013-06-27 18:28:16 -0500

    Changes for support asynchronous communication

--- app/proyectosmtv/proyectomtv-controller.js
+++ app/proyectosmtv/proyectomtv-controller.js
@@ -1,37 +1,10 @@
 define(['angular'], function(angular) {
   'use strict';
 
-  angular.module('ProyectoCtrl', []).controller('ProyectoCtrl', ['caliopewebTemplateSrv','$scope', '$routeParams', function (service, $scope, $routeParams) {
-    
-    var caliopeForm = new Object(); 
-    
-    caliopeForm.id = $routeParams.plantilla;
-    caliopeForm.mode = $routeParams.mode; 
-    $scope.caliopeForm = caliopeForm;
-    
-    alert('ProyectoCtrl');
-    $scope.jsonPlantilla = service.load(caliopeForm);
-    
-    $scope.create = function () {
-      var id = $scope.proyecto.id;
-      var nombre = $scope.proyecto.nombre;  
-      
-      alert(id + ' ' + nombre);
-      //proyectoSrv.create(id, nombre); 
-    };  
-    
-    
-  }]);
+  var moduleControllers = angular.module('ProyectoControllers', []);
   
-  /*
-  moduleangular.module('ProyectoCtrl').controller('customerList', ['MyService', function(MyService){
-    $scope.customers = MyService.getCustomers();
-  }]);
-  
-  var module = angular.module('ProyectoCtrl', []);
-  
-  module.controller('ProyectoCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
-    
+  moduleControllers.controller('ProyectoCtrl', ['caliopewebTemplateSrv','$scope', '$routeParams', function (service, $scope, $routeParams) {
+       
     $scope.create = function () {
       var id = $scope.proyecto.id;
       var nombre = $scope.proyecto.nombre;  
@@ -40,10 +13,5 @@ define(['angular'], function(angular) {
       //proyectoSrv.create(id, nombre); 
     };  
         
-  }]);
-  
-  moduleangular.module('ProyectoCtrl').controller('customerList', ['MyService', function(MyService){
-    $scope.customers = MyService.getCustomers();
-  }]);
-  */  
+  }]); 
 });
