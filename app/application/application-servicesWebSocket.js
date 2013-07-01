ee7edfd2f1b7cb970d567a98a04383f78a8852ff
ndochoa <ndaniel8a@gmail.com>
2013-06-30 22:13:52 -0500

    Adding a new Class to WebSocksetCaliope and creation of array of web sockets with different configs. Adding a new Service for get the array of web sockets, and method for init the websockets.

--- app/application/application-servicesWebSocket.js
+++ app/application/application-servicesWebSocket.js
@@ -2,127 +2,97 @@ define(['angular'], function(angular) {
   'use strict';
   angular.module('webSocket', []).factory('webSocket', 
       ['$q', '$rootScope', function($q, $rootScope) {
-        
-    var Service = {};   
-
-    Service.WebSocketCaliope = function(Url) {
-      var ws = new WebSocket(Url);
-      
-      ws.onopen = function(){
-        console.log("Socket has been opened! ", ws.url);
-      };
     
-      ws.onmessage = function(message) {
-        listener(JSON.parse(message.data));
-      }; 
+      var Service = {}; 
+      var webSockets = {};
+      initWebSockets();   
+        
+      function initWebSockets() {                
+        var wsTemplates = new WebSocketCaliope(
+            "ws://127.0.0.1:8080/WebSocketTest/Templates"
+          );
+        var wsLogin = new WebSocketCaliope(
+            "ws://127.0.0.1:8080/WebSocketTest/Templates"
+          );
+        webSockets.templates = wsTemplates;
+        webSockets.login = wsLogin;
+      }
       
-      var sendRequest = function (request) {
-        var defer = $q.defer();
-        var callbackId = getCallbackId();
-        callbacks[callbackId] = {
-          time: new Date(),
-          cb:defer
+      function WebSocketCaliope(Url) {
+        
+        // Keep all pending requests here until they get responses
+        var callbacks = {};
+        // Create a unique callback ID to map requests to responses
+        var currentCallbackId = 0;
+        
+        var ws = new WebSocket(Url);
+        var url = "url";
+        this.sendRequest = sendRequest;
+        this.getStatus = getStatus;
+        this.getUrl = getUrl;
+        
+        
+        ws.onopen = function(){
+          console.log("Socket has been opened! ", ws.url);
         };
-        request.callback_id = callbackId;
-        console.log('Sending request', request);
-        ws.send(JSON.stringify(request));
-        return defer.promise;
-      }
-    
-      var listener = function (data) {
-        var messageObj = data;
-        console.log("Received data from websocket: ", messageObj);
-        // If an object exists with callback_id in our callbacks object, resolve it
-        if(callbacks.hasOwnProperty(messageObj.callback_id)) {
-          $rootScope.$apply(callbacks[messageObj.callback_id].cb.resolve(messageObj.data));
-          delete callbacks[messageObj.callbackID];
-        }
-      }
-      // This creates a new callback ID for a request
-      var getCallbackId = function () {
-        currentCallbackId += 1;
-        if(currentCallbackId > 10000) {
-          currentCallbackId = 0;
-        }
-        return currentCallbackId;
-      } 
       
-      return this;
-    }
-    
-    Service.WebSockets = function() {
-      var config = "esto";
-      /*
-          templates : new WebSocketCaliope("ws://127.0.0.1:8080/WebSocketTest/Templates"),
-          templatesLogin : new WebSocketCaliope("ws://127.0.0.1:8080/WebSocketTest/Templates1")
-      };
-      */
-      var getConfig = function() {
-        return this.config;
-      }
-      
-      return this;
-    }
+        ws.onmessage = function(message) {
+          listener(JSON.parse(message.data));
+        }; 
         
-
-    // Keep all pending requests here until they get responses
-    var callbacks = {};
-    // Create a unique callback ID to map requests to responses
-    var currentCallbackId = 0;
-    // Create our websocket object with the address to the websocket
-    var ws = {};
-    
-    var ws = new WebSocket("ws://127.0.0.1:8080/WebSocketTest/Templates");
-    
-    
-    ws.onopen = function(){
-      console.log("Socket has been opened! ", ws.url);
-    };
-
-    ws.onmessage = function(message) {
-      listener(JSON.parse(message.data));
-    }; 
-    
-//    Service.openWebSocket = function(URL) {
-//   
-//      return ws;
-//    }
-
-
-    
-
-    Service.sendRequest = function(request) {
-      var defer = $q.defer();
-      var callbackId = getCallbackId();
-      callbacks[callbackId] = {
-        time: new Date(),
-        cb:defer
-      };
-      request.callback_id = callbackId;
-      console.log('Sending request', request);
-      ws.send(JSON.stringify(request));
-      return defer.promise;
-    }
-
-    function listener(data) {
-      var messageObj = data;
-      console.log("Received data from websocket: ", messageObj);
-      // If an object exists with callback_id in our callbacks object, resolve it
-      if(callbacks.hasOwnProperty(messageObj.callback_id)) {
-        $rootScope.$apply(callbacks[messageObj.callback_id].cb.resolve(messageObj.data));
-        delete callbacks[messageObj.callbackID];
+        ws.onerror = function() {
+          
+        }
+        
+        function getStatus() {
+          return ws.readyState; 
+        }
+        
+        function getUrl() {
+          return url;
+        }
+        
+        function sendRequest (request) {
+          var defer = $q.defer();
+          var callbackId = getCallbackId();
+          callbacks[callbackId] = {
+            time: new Date(),
+            cb:defer
+          };
+          request.callback_id = callbackId;
+          console.log('Sending request', request);
+          ws.send(JSON.stringify(request));
+          return defer.promise;
+        }
+      
+        var listener = function (data) {
+          var messageObj = data;
+          console.log("Received data from websocket: ", messageObj);
+          // If an object exists with callback_id in our callbacks object, resolve it
+          if(callbacks.hasOwnProperty(messageObj.callback_id)) {
+            $rootScope.$apply(
+                callbacks[messageObj.callback_id].cb.resolve(messageObj.data)
+              );
+            delete callbacks[messageObj.callbackID];
+          }
+        }
+        // This creates a new callback ID for a request
+        var getCallbackId = function () {
+          currentCallbackId += 1;
+          if(currentCallbackId > 10000) {
+            currentCallbackId = 0;
+          }
+          return currentCallbackId;
+        } 
+        
       }
-    }
-    // This creates a new callback ID for a request
-    function getCallbackId() {
-      currentCallbackId += 1;
-      if(currentCallbackId > 10000) {
-        currentCallbackId = 0;
-      }
-      return currentCallbackId;
-    }         
     
-    return Service;
+      Service.WebSockets = function() {
+        return webSockets;
+      }
+    
+   
+      return Service;
 
   }])
 });
