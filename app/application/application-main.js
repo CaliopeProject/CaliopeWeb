a604469b8e307978c3d7c24af23ed954e99c4a30
aurigadl <aurigadl@gmail.com>
2013-07-04 12:02:08 -0500

    fix error dform

--- app/application/application-main.js
+++ app/application/application-main.js
@@ -1,33 +1,33 @@
 require.config({
-  paths: {    
-    jquery                           : 'libs-js-thirdparty/jquery/jquery',
-    dform                            : 'libs-js-thirdparty/jquery.dform/dist/jquery.dform-1.1.0',
-    angular                          : 'libs-js-thirdparty/angular-unstable/angular',
-    uuid                             : 'libs-js-thirdparty/uuid-js/lib/uuid',
-    Crypto                           : 'libs-js-thirdparty/cryptojs/lib/Crypto',    
-    CryptoSHA256                     : 'libs-js-thirdparty/cryptojs/lib/SHA256',    
+  paths: {
+    'jquery'                           : 'libs-js-thirdparty/jquery/jquery',
+    'dform'                            : 'libs-js-thirdparty/jquery.dform/dist/jquery.dform-1.1.0',
+    'angular'                          : 'libs-js-thirdparty/angular-unstable/angular',
+    'uuid'                             : 'libs-js-thirdparty/uuid-js/lib/uuid',
+    'Crypto'                           : 'libs-js-thirdparty/cryptojs/lib/Crypto',
+    'CryptoSHA256'                     : 'libs-js-thirdparty/cryptojs/lib/SHA256',
     'application-app'                : 'application/application-app',
     'application-servicesWebSocket'  : 'application/application-servicesWebSocket',
     'application-controller'         : 'application/application-controller',
     'application-routes'             : 'application/application-routes',
-    'caliopeweb-templateServices'    : 'caliopeweb-forms/caliopeweb-template-services',    
+    'caliopeweb-templateServices'    : 'caliopeweb-forms/caliopeweb-template-services',
     'caliopeweb-templateControllers' : 'caliopeweb-forms/caliopeweb-template-controllers',
     'caliopeweb-formDirectives'      : 'caliopeweb-forms/caliopeweb-form-directives',
     'proyectosmtv-controller'        : 'proyectosmtv/proyectomtv-controller',
     'proyectosmtv-service'           : 'proyectosmtv/proyectomtv-service',
     'login-services'                 : 'login/login-services',
-    'login-controllers'              : 'login/login-controllers',    
+    'login-controllers'              : 'login/login-controllers',
     'application-routes'             : 'application/application-routes'
   },
   baseUrl: '/',
   shim: {
     'jquery'                         : {'exports' : 'jquery'},
-    'jquery'                         : {'exports' : 'jQuery'},
     'angular'                        : {'exports' : 'angular'},
     'application-app'                : {'exports' : 'app'},
     'application-servicesWebSocket'  : {'exports' : 'webSocket'},
-    'application-routes'             : {'exports' : 'routes'},    
-    'application-controller'         : {'exports' : 'appcontroller'},    
+    'application-routes'             : {'exports' : 'routes'},
+    'application-controller'         : {'exports' : 'appcontroller'},
+    'dform'                          : {deps      : ['jquery']},
     'angularMocks'                   : {deps      : ['angular'], 'exports' : 'angular.mock'}
   },
   priority: [
@@ -38,14 +38,14 @@ require.config({
 require([
   'jquery',
   'dform',
-  'Crypto',  
+  'Crypto',
   'CryptoSHA256',
-  'angular',   
+  'angular',
   'application-app',
   'application-routes'
-], function($, dform, crypto, CryptoSHA256, angular, app, routes) {
+], function(jQuery, dform, crypto, CryptoSHA256, angular, app, routes) {
   'use strict';
-  $(document).ready(function () {    
+  $(document).ready(function () {
     var $html = $('html');
     angular.bootstrap($html, [app['name']]);
     // Because of RequireJS we need to bootstrap the app app manually
