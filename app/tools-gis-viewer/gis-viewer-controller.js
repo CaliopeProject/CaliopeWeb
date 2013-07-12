56050db8511e2c41915a4598d081ef0513c4c3e0
Luis Romero <lucien.luisromero@gmail.com>
2013-07-12 09:39:43 -0500

    Removed .idea files  from repository

--- app/tools-gis-viewer/gis-viewer-controller.js
+++ app/tools-gis-viewer/gis-viewer-controller.js
@@ -6,11 +6,12 @@ define(['angular', 'gis-ext-base','gis-heron'], function(angular) {
   moduleControllers.controller('GisViewerController', 
       ['$scope', '$routeParams', 
       function ($scope, $routeParams) {
-			
-	var bounds = new OpenLayers.Bounds(
-        -74.221, 4.471,
-        -74.008, 4.849
-    );
+
+
+          var bounds = new OpenLayers.Bounds(
+              -74.221, 4.471,
+              -74.008, 4.849
+          );
 	  
 	  Ext.namespace('Heron.options.map');
 		Heron.options.map.settings = {
@@ -20,39 +21,49 @@ define(['angular', 'gis-ext-base','gis-heron'], function(angular) {
 			zoom: 0
 		};
 		Heron.options.map.layers = [
-		
-				  new OpenLayers.Layer.WMS("Orto Imagen IDECA",
-				  "http://imagenes.catastrobogota.gov.co/arcgis/services/Ortho2010/MapServer/WMSServer?",
-				  {layers: "0"})/*,
-                        	new OpenLayers.Layer.WMS("Area Territorial",
-								  "http://mapas.catastrobogota.gov.co/arcgiswsh/Mapa_Referencia/Mapa_referencia/MapServer/WMSServer?",
-				  {layers: "4"})*/
-                        ];
-                        Heron.options.map.toolbar = [
-                        	{type: "scale", options: {width: 110}},
-                        	{type: "-"} ,
-                        	{type: "featureinfo", options: {popupWindow: {}}},
-                        	{type: "-"} ,
-                        	{type: "pan"},
-                        	{type: "zoomin"},
-                        	{type: "zoomout"},
-                        	{type: "zoomvisible"},
-                        	{type: "-"} ,
-                        	{type: "zoomprevious"},
-                        	{type: "zoomnext"},
-                        	{type: "-"},
-                        	{type: "measurelength", options: {geodesic: true}},
-                        	{type: "measurearea", options: {geodesic: true}},
-                        	{type: "capas",
+            new OpenLayers.Layer.WMS("Orto Imagen IDECA",
+                "http://imagenes.catastrobogota.gov.co/arcgis/services/Ortho2010/MapServer/WMSServer?",
+				  {
+                      layers: "0",
+                      format: "image/png"
+                  }
+            ),
+            new OpenLayers.Layer.WMS("Area Territorial",
+                "http://mapas.catastrobogota.gov.co/arcgiswsh/Mapa_Referencia/Mapa_referencia/MapServer/WMSServer?",
+                {
+                    layers: "4",
+                    format: "image/png",
+                    transparent: true
+                },
+                {
+                    isBaseLayer:false
+                }
+            )
+        ];
+        Heron.options.map.toolbar = [
+            {type: "scale", options: {width: 110}},
+            {type: "-"} ,
+            {type: "featureinfo", options: {popupWindow: {}}},
+            {type: "-"} ,
+            {type: "pan"},
+            {type: "zoomin"},
+            {type: "zoomout"},
+            {type: "zoomvisible"},
+            {type: "-"} ,
+            {type: "zoomprevious"},
+            {type: "zoomnext"},
+            {type: "-"},
+            {type: "measurelength", options: {geodesic: true}},
+            {type: "measurearea", options: {geodesic: true}},
+            {type: "capas",
 					options: {
-
 						text: 'Capas',
 						iconCls: 'bmenu',
 						handler: function () {
 							    layerTreeDialog= new Ext.Window({
 							    layout: "fit",
 							    width: 350,
-							    autoHeight: true,
+                                height: 500,
 							    items: [{								
 								 xtype: 'hr_layertreepanel'
 								}
@@ -65,61 +76,13 @@ define(['angular', 'gis-ext-base','gis-heron'], function(angular) {
 				
                         ];
 		Heron.layout = {
-	xtype: 'hr_mappanel',
-	renderTo: 'siim_mapdiv',
-	height: 400,
-	width: '100%',
-	hropts: Heron.options.map
-	};
-        /*
-		Ext.namespace("Heron");
-		var bounds = new OpenLayers.Bounds(
-				-81.5625,2.8125,-78.75,5.625
-			);
-        
-		Ext.namespace("Heron.options.map");
-       Heron.options.map.settings = {
-	projection: 'EPSG:4326',
-	units: 'degrees',
-	maxExtent: bounds,
-	xy_precision: 3,
-	max_features: 10,
-	zoom: 1,
-	theme: null
-};
+            xtype: 'hr_mappanel',
+            renderTo: 'siim_mapdiv',
+            height: 400,
+            width: '100%',
+            hropts: Heron.options.map
+        };
 
-Heron.options.map.layers = [
-
-
-                        	new OpenLayers.Layer.WMS("Global Imagery",
-								  "http://maps.opengeo.org/geowebcache/service/wms",
-				  {layers: "bluemarble"})
-                        	
-                        ];
-                        Heron.options.map.toolbar = [
-                        	{type: "scale", options: {width: 110}},
-                        	{type: "-"} ,
-                        	{type: "featureinfo", options: {popupWindow: {}}},
-                        	{type: "-"} ,
-                        	{type: "pan"},
-                        	{type: "zoomin"},
-                        	{type: "zoomout"},
-                        	{type: "zoomvisible"},
-                        	{type: "-"} ,
-                        	{type: "zoomprevious"},
-                        	{type: "zoomnext"},
-                        	{type: "-"},
-                        	{type: "measurelength", options: {geodesic: true}},
-                        	{type: "measurearea", options: {geodesic: true}}
-                        ];
-		Heron.layout = {
-	xtype: 'hr_mappanel',
-	renderTo: 'mapdiv',
-	height: 400,
-	width: '100%',
-	hropts: Heron.options.map
-	};
-		*/
 		Heron.App.create();
 		Heron.App.show();
       }
