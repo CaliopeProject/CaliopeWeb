88038a957625c46c87e497ff618c520525f2d225
aurigadl <aurigadl@gmail.com>
2013-07-15 13:58:27 -0500

    delete files that are not needed in the style sheet.

--- app/menu-top/menu-top-controllers.js
+++ app/menu-top/menu-top-controllers.js
@@ -3,33 +3,13 @@
 
 define(['angular','angular-ui-bootstrap-bower'], function(angular) {
   'use strict';
-  var moduleControllers = angular.module('MenuTopControllers', []);
-
-
-  moduleControllers.constant('messageGeneric', {
-    administrador: 'Por favor informe al administrador del sistema'
-  });
+  var moduleControllers = angular.module('MenuTopControllers', ['ui.bootstrap']);
 
   moduleControllers.controller('alertCtrl',['$scope','HandlerResponseServerSrv',
     function($scope, handlerResServerSrv){
 
-      $scope.$on('ChangeTextAlertMessage', function(text) {
-        console.log('Onxx ChangeTextAlertMessage', text);
-        console.log('servcioNotifica',handlerResServerSrv);
-        $scope.alertMessage = text;
-      });
-    
-      $scope.$watch('processingResponse', function(value){
-        console.log('Change processingResponse', value);
-        if(value === true){
-          $scope.alertMessage = 'Procesando';
-        }else{
-          //$scope.alertMessage = '';
-        }
-      });
+      //insert text body
 
-      $scope.processingResponse  = false;
-      $scope.alertMessage        = 'hola mundo';
     }
   ]);
 });
