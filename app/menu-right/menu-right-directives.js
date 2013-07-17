bc46444e414651b4d7082caf6b8a03369548329c
aurigadl <aurigadl@gmail.com>
2013-07-17 18:09:56 -0500

    structure application new element in partial content

--- app/menu-right/menu-right-directives.js
+++ app/menu-right/menu-right-directives.js
@@ -16,10 +16,25 @@ define(['angular'], function(angular) {
     }
   ]);
 
-  moduleControllers.directive('sidebar',['$scope',
+  moduleControllers.directive('items',['$scope',
     function($scope){
       return {
         restrict: 'A',
+        link: function (scope, elem, attrs) {
+          console.log("Recognized the fundoo-rating directive usage");
+        }
+      };
+    }
+  ]);
+
+  moduleControllers.directive('shortcuts',['$scope',
+    function($scope){
+      return {
+        restrict: 'A',
+        template: '',
+        scope: {
+          msg: '='
+        },
         link: function (scope, elem, attrs) {
           console.log("Recognized the fundoo-rating directive usage");
         }
