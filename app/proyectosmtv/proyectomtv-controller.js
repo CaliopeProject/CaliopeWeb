1bc89824fcbb56d3499add65e3d0b3a50f76a12d
aurigadl <aurigadl@gmail.com>
2013-07-26 11:17:44 -0500

    new query parameters for tasks

--- app/proyectosmtv/proyectomtv-controller.js
+++ app/proyectosmtv/proyectomtv-controller.js
@@ -2,20 +2,20 @@ define(['angular'], function(angular) {
   'use strict';
 
   var moduleControllers = angular.module('ProyectoControllers', []);
-  
-  moduleControllers.controller('ProyectomtvCtrl', 
-      ['caliopewebTemplateSrv','$scope', '$routeParams', 
+
+  moduleControllers.controller('ProyectomtvCtrl',
+      ['caliopewebTemplateSrv','$scope', '$routeParams',
        function (caliopeWebTemplateSrv, $scope, $routeParams) {
-       
+
         $scope.create = function () {
-          var formAng = $scope[$scope.caliopeForm.id];         
-          var inputs = $scope.inputsFormTemplate;          
+          var formAng = $scope[$scope.caliopeForm.id];
+          var inputs = $scope.inputsFormTemplate;
           var obj = {};
           for( var i = 0; i < inputs.length; i++) {
             obj[inputs[i]] = $scope[inputs[i]];
-          }                              
+          }
           caliopeWebTemplateSrv.sendDataForm(obj, $scope.caliopeForm.id);
-        };         
+        };
       }]
-  ); 
+  );
 });
