7dfaa9885885f1dea11e083bc4ec9776890f04c7
Luis Romero <lucien.luisromero@gmail.com>
2013-07-26 16:09:34 -0500

    update proxy url por WMSGetFeatureInfo component

--- app/tools-gis-viewer/gis-viewer-controller.js
+++ app/tools-gis-viewer/gis-viewer-controller.js
@@ -165,7 +165,7 @@ define(['angular', 'gis-ext-base','gis-heron'], function(angular) {
             )
             ,
             new OpenLayers.Layer.WMS("Lotes",
-                "http://localhost:9000/catastrobogota/WMSServer",
+                "http://localhost:9000/gis_proxy/WMSServer",
                 {
                     layers: "14",
                     format: "image/png",
@@ -195,7 +195,7 @@ define(['angular', 'gis-ext-base','gis-heron'], function(angular) {
           });
 
           var featureInfoControl = new OpenLayers.Control.WMSGetFeatureInfo({
-              url: 'http://localhost:9000/catastrobogota/WMSServer',
+              url: 'http://localhost:9000/gis_proxy/WMSServer',
               title: 'Identificar elementos',
               layers: [Heron.options.map.layers[5]],
               queryVisible: true,
