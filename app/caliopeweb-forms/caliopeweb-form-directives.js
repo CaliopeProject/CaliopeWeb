fc823837ba23373d42b340a30e681aa01f5726fe
aurigadl <aurigadl@gmail.com>
2013-08-06 18:20:31 -0500

    multicombobox runing

--- app/caliopeweb-forms/caliopeweb-form-directives.js
+++ app/caliopeweb-forms/caliopeweb-form-directives.js
@@ -216,5 +216,85 @@ define(['angular', 'dform'], function (angular) {
 
     return directiveDefinitionObject;
   }]);
+
+  moduleDirectives.directive("uiMcomboChoices", function($document){
+    // simultaneously should not be two open items
+    var openElement = null, close;
+    return {
+      restrict: 'A',
+      replace: true,
+      templateUrl: 'caliopeweb-forms/caliopeweb-mcombo-partial-directive.html',
+      scope: {
+        _choices         : '=uiMcomboChoices',
+        _selectedChoices : '=uiMcomboSelected'
+      },
+      controller: ['$scope', '$filter', function($scope, $filter) {
+        $scope._searchElem = null;
+
+        $scope.filteredChoices = function() {
+          var filtered = $filter('filter')($scope._choices, $scope._search);
+          return $filter('orderBy')(filtered, 'text');
+        };
+
+        $scope.moveToSelected = function(choice, $event) {
+          $scope._selectedChoices.push(choice);
+          $scope._choices.splice($scope._choices.indexOf(choice), 1);
+          $scope._search='';
+
+          $scope._searchElem.focus();
+
+          // do not 'close' on choice click
+          $event.preventDefault();
+          $event.stopPropagation();
+        };
+
+        $scope.removeFromSelected = function(choice) {
+          $scope._choices.push(choice);
+          $scope._selectedChoices.splice($scope._selectedChoices.indexOf(choice), 1);
+
+          $scope._searchElem.focus();
+        };
+      }
+      ],
+      link: function(scope, element, attrs) {
+        var selUl = element.children().eq(0);
+        var selItems = selUl.children();
+        scope._searchElem = selItems.eq(selItems.length-1).children().eq(0)[0];
+        selUl.bind('click', function(event) {
+          // otherwise 'close' will be called immediately
+          event.preventDefault();
+          event.stopPropagation();
+
+          if (openElement) {
+            close();
+          }
+
+          if (!element.hasClass('mcombo-container-active')) {
+            element.addClass('mcombo-container-active');
+            openElement = element;
+
+            close = function (event) {
+
+              if(event){
+                event.preventDefault();
+              }
+
+              if(event){
+                event.stopPropagation();
+              }
+
+              $document.unbind('click', close);
+              element.removeClass('mcombo-container-active');
+              close = null;
+              openElement = null;
+            };
+            $document.bind('click', close);
+          }
+
+          scope._searchElem.focus();
+        });
+      }
+    };
+  });
 });
 
