7894f6570c5170bad1938761e0130f093228b567
aurigadl <aurigadl@gmail.com>
2013-08-13 09:38:07 -0500

    runing kanban and try connection with server

--- app/tools-kanban-board/kanban-board-controller.js
+++ app/tools-kanban-board/kanban-board-controller.js
@@ -4,68 +4,89 @@
 
 define(['angular','angular-dragdrop'], function (angular) {
   'use strict';
-var dirmodule = angular.module('kanbanBoardCtrl', ['login-security-services','ngDragDrop']);
-dirmodule.controller("kanbanBoardCtrl",
-  ["SessionSrv", "$scope","webSocket", "$log", 'taskService',
-  function(security, $scope, webSocket, $log, taskService) {
-    var uuid, tasks;
+  var dirmodule = angular.module('kanbanBoardCtrl', ['login-security-services','ngDragDrop']);
 
-    $scope.$on('openWebSocket', function(event, data) {
-      uuid = security.getIdSession();
-      //$scope.uuid    = uuid;
-    });
+  dirmodule.controller("kanbanBoardCtrl",
+    ["SessionSrv", "$scope","webSocket", "$log", 'taskService', 'caliopewebTemplateSrv',
+      function(security, $scope, webSocket, $log, taskService, tempServices) {
 
-    function updateKanban(uuid) {
-      var params = {};
-      var method = "tasks.getAll";
+        var uuid, tasks;
+        var webSockets = webSocket.WebSockets();
 
-      params = {
-        "uuid" : uuid
-      };
+        $scope.$on('openWebSocket', function(event, data) {
+          uuid = security.getIdSession();
+        });
 
-    var webSockets = webSocket.WebSockets();
-    webSockets.serversimm.sendRequest(method, params).then(function(data){
-     data[1] = ({"category":"Doing","tasks":[{"tarea":null,"uuid":"99cc7bdb-a8dc-4511-a94e-7f637c08148b","description":"dfasdfasfasdf\nadfasdf\nadsf asdf ads\nf"},{"tarea":null,"uuid":"860ffd57-672c-4491-ss86-62fe5a7f09bf","description":null}]});
-     data[2] = ({"category":"Doing","tasks":[{"tarea":null,"uuid":"99cc7bdb-a8dc-4511-a94e-7f637c08148b","description":"dfasdfasfasdf\nadfasdf\nadsf asdf ads\nf"},{"tarea":null,"uuid":"860ffd57-672c-4491-ss86-62fe5a7f09bf","description":null}]});
+        function updateKanban(uuid) {
+          var params = {};
+          var method = "tasks.getAll";
 
-     $scope.data = data;
-    });
+          params = {
+            "uuid" : uuid
+          };
 
-      $scope.deleteTask = function( uuid ) {
-        taskService.deleteTask(uuid);
-      };
-    }
+          webSockets.serversimm.sendRequest(method, params).then(function(data){
+            data[1] = ({"category":"Doing","tasks":[{"tarea":null,"uuid":"99cc7bdb-a8dc-4511-a94e-7f637c08148b","description":"dfasdfasfasdf\nadfasdf\nadsf asdf ads\nf"},{"tarea":null,"uuid":"860ffd57-672c-4491-ss86-62fe5a7f09bf","description":'la descrip 03'}]});
+            $scope.data = data;
+          });
 
-    $scope.$on('updateKanban', function(event, data) {
-      updateKanban(uuid);
-    });
+          $scope.deleteTask = function( uuid ) {
+            taskService.deleteTask(uuid);
+          };
+        }
 
-    updateKanban(uuid);
+        $scope.$on('updateKanban', function(event, data) {
+          updateKanban(uuid);
+        });
 
-    $scope.startCallback = function(event, ui) {
-      console.log('You started draggin');
-    };
+        updateKanban(uuid);
 
-    $scope.stopCallback = function(event, ui) {
-      console.log('Why did you stop draggin me?');
-    };
+        $scope.dropCallback = function(event, ui) {
 
-    $scope.dragCallback = function(event, ui) {
-      console.log('hey, look I`m flying');
-    };
+          var category , categ, uuid, findCateg, itemsbycateg, data;
+          var params = {};
+          var method = "tasks.edit";
 
-    $scope.dropCallback = function(event, ui) {
-      console.log('hey, you dumped me :-(');
-    };
+          itemsbycateg = (function(){
+            var i,j,categoryUiid = [];
+            for(i=0; $scope.data.length > i; i++){
+              for(j=0; $scope.data[i].tasks.length > j; j++){
+                if($scope.data[i].tasks[j].uuid === undefined){
+                  $scope.data[i].tasks.splice(j, 1);
+                }else{
+                  categoryUiid.push({uuid : $scope.data[i].tasks[j].uuid , categ :$scope.data[i].category});
+                }
+              }
+            }
 
-    $scope.overCallback = function(event, ui) {
-      console.log('Look, I`m over you');
-    };
+            return categoryUiid;
+          }());
 
-    $scope.outCallback = function(event, ui) {
-      console.log('I`m not, hehe');
-    };
+          findCateg = function (array, value) {
+            var i, items;
+            console.log('pase');
+            for(i = 0; i<array.length; i++) {
+                if(array[i].uuid === value){
+                  return array[i].categ;
+                }
+            }
+          };
 
+          uuid         = ui.draggable.attr("uuid");
+          categ        = findCateg(itemsbycateg, uuid);
 
-  }]);
+          console.log('uuid',uuid);
+          console.log('cagt',categ);
+
+          if(categ !== undefined){
+            data  = {
+              "uuid"     : uuid,
+              "category" : categ
+            };
+            tempServices.sendDataForm('asignaciones', 'tasks.edit', data, '', uuid );
+          }
+          // webSockets.serversimm.sendRequest(method, params);
+        };
+
+      }]);
 });
