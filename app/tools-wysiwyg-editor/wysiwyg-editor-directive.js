670db26ea88ddc6ee5fbd735fb5367311ec0d15b
ndochoa <ndaniel8a@gmail.com>
2013-08-28 16:12:52 -0500

    Integration of ckEdit in dynamic forms

--- app/tools-wysiwyg-editor/wysiwyg-editor-directive.js
+++ app/tools-wysiwyg-editor/wysiwyg-editor-directive.js
@@ -15,75 +15,132 @@ dirmodule.directive('ckedit', function ($parse) {
         templateUrl: 'tools-wysiwyg-editor/wysiwyg-editor-directive.html',
         replace: true,
         restrict: 'E',
+        require: '?ngModel',
         
-        link: function (scope, element, attrs, controller) {
-            var getter = $parse(attrs.ckedit), 
-                setter = getter.assign;
-      
-            console.log(element)
-            attrs.$set('contenteditable', true); // inline ckeditor needs this
-            if (!attrs.id) {
-                attrs.$set('id', prefix + (++counter));
+        link: function (scope, element, attrs, ngModel) {
+
+          // Write data to the model
+          function read() {
+            var html = element.html();
+            // When we clear the content editable the browser leaves a <br> behind
+            // If strip-br attribute is provided then we strip this out
+            if( attrs.stripBr && html == '<br>' ) {
+              html = '';
             }
+            ngModel.$setViewValue(html);
+          }
 
-            // CKEditor stuff
-            // Override the normal CKEditor save plugin
-
-            CKEDITOR.plugins.registered['save'] =
-            {
-                init: function (editor) {
-                    editor.addCommand('save',
-                        {
-                            modes: { wysiwyg: 1, source: 1 },
-                            exec: function (editor) {
-                                if (editor.checkDirty()) {
-                                    var ckValue = editor.getData();
-                                    scope.$apply(function () {
-                                        setter(scope, ckValue);
-                                    });
-                                    ckValue = null;
-                                    editor.resetDirty();
-                                }
-                            }
-                        }
-                    );
-                    editor.ui.addButton('Save', { label: 'Save', command: 'save', toolbar: 'document' });
-                }
+          if( ngModel !== undefined ) {
+            // Specify how UI should be updated
+            ngModel.$render = function() {
+              element.html(ngModel.$viewValue || '');
             };
-            var options = {
-                toolbar : [
-                            { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ] },
-                            { name: 'insert', items: [ 'Image','Link' ] },
-                            { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] }
-                          ],
-                toolbarGroups: [
-                            { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-                            { name: 'insert' },
-                            { name: 'clipboard', groups: [ 'clipboard', 'undo' ] }
-                        ]
-                };
-            options.on = {
-                blur: function (e) {
-                    if (e.editor.checkDirty()) {
-                        var ckValue = e.editor.getData();
-                        scope.$apply(function () {
-                            setter(scope, ckValue);
-                        });
-                        ckValue = null;
-                        e.editor.resetDirty();
-                    }
-                }
-            };
-            options.removePlugins = 'sourcearea';
-            var editorangular = CKEDITOR.inline(element[0], options); //invoke
 
-            scope.$watch(attrs.ckedit, function (value) {
-                editorangular.setData(value);
-                console.log(value)
+            // Listen for change events to enable binding
+            element.on('blur keyup change', function() {
+              scope.$apply(read);
             });
+            read(); // initialize
+          }
+
+          var getter = $parse(attrs.ckedit),
+              setter = getter.assign;
+
+
+          if( ngModel !== undefined ) {
+            // Specify how UI should be updated
+            ngModel.$render = function() {
+              element.html(ngModel.$viewValue || '');
+            };
+
+            // Listen for change events to enable binding
+            element.on('blur keyup change', function() {
+              scope.$apply(read);
+            });
+            read(); // initialize
+          }
+
+          if (!attrs.id) {
+              attrs.$set('id', prefix + (++counter));
+          }
+
+          // CKEditor stuff
+          // Override the normal CKEditor save plugin
+
+          CKEDITOR.plugins.registered['save'] =
+          {
+              init: function (editor) {
+                  editor.addCommand('save',
+                      {
+                          modes: { wysiwyg: 1, source: 1 },
+                          exec: function (editor) {
+                              if (editor.checkDirty()) {
+                                  var ckValue = editor.getData();
+                                  scope.$apply(function () {
+                                      setter(scope, ckValue);
+                                  });
+                                  ckValue = null;
+                                  editor.resetDirty();
+                              }
+                          }
+                      }
+                  );
+                  editor.ui.addButton('Save', { label: 'Save', command: 'save', toolbar: 'document' });
+              }
+          };
+          var options = {
+              toolbar : [
+                          { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ] },
+                          { name: 'insert', items: [ 'Image','Link' ] },
+                          { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] }
+                        ],
+              toolbarGroups: [
+                          { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+                          { name: 'insert' },
+                          { name: 'clipboard', groups: [ 'clipboard', 'undo' ] }
+                      ]
+              };
+
+          options.removePlugins = 'sourcearea';
+          CKEDITOR.inline(element[0], options);
+
         }
     }
     
 });
+
+dirmodule.directive('contenteditable1', function() {
+    return {
+      restrict: 'A', // only activate on element attribute
+      require: '?ngModel', // get a hold of NgModelController
+      link: function(scope, element, attrs, ngModel) {
+        console.log('ngModel contenteditable1', ngModel)
+        if(!ngModel) return; // do nothing if no ng-model
+
+        // Specify how UI should be updated
+        ngModel.$render = function() {
+          element.html(ngModel.$viewValue || '');
+        };
+
+        // Listen for change events to enable binding
+        element.on('blur keyup change', function() {
+          scope.$apply(read);
+        });
+        read(); // initialize
+
+        // Write data to the model
+        function read() {
+          var html = element.html();
+          // When we clear the content editable the browser leaves a <br> behind
+          // If strip-br attribute is provided then we strip this out
+          if( attrs.stripBr && html == '<br>' ) {
+            html = '';
+          }
+          ngModel.$setViewValue(html);
+        }
+      }
+    };
+  });
+
 
 });
