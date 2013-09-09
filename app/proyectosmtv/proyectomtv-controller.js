5cc8e1202bf46815bacb8c55a4f8a3051c79a615
ndochoa <ndaniel8a@gmail.com>
2013-09-09 10:10:12 -0500

    Remove code example for second grid.

--- app/proyectosmtv/proyectomtv-controller.js
+++ app/proyectosmtv/proyectomtv-controller.js
@@ -5,11 +5,10 @@ define(['angular'], function(angular) {
 
   moduleControllers.controller('ProyectomtvCtrl',
     ['caliopewebTemplateSrv', 'caliopewebGridSrv', '$scope', '$routeParams',
-    function (cwTemplateService, cwGridService, $scope, $routeParams) {
+    function (cwTemplateService, cwGridService, $scope) {
 
 
       $scope.initGrid = function() {
-
 
         var cwGrid = $scope['gridProjectsmtv'];
         cwGrid.addColumn("name", {"name": "Nombre", "show" : true});
@@ -27,21 +26,7 @@ define(['angular'], function(angular) {
 
       };
 
-      $scope.initGrid1 = function() {
-        var cwGrid1 = $scope['gridProjectsmtv1'];
-        cwGrid1.addColumn("name", {"name": "Nombre", "show" : true});
-        cwGrid1.addColumn("locality", {"name": "Localidad", "show" : true});
-        cwGrid1.addColumn("uuid", {"name": "Id", "show" : false});
-        cwGrid1.addColumn("tree", {"name": "Arbol", "show" : true});
-        cwGrid1.addColumn("Kanban", {"name": "Kanban", "show" : true});
-        cwGrid1.addColumn("actions", {"name": "Acciones", "show" : true, "width" : 200, "class" : "cell-center"});
 
-        cwGrid1.addColumnProperties("actions", {
-          "htmlContent": '<widget-task category="ToDo" parent="{{row.entity.uuid}}"/>'
-        });
-
-        cwGrid1.setDecorators([CaliopeWebGridDataDecorator, CWGridColumnsDefNgGridDecorator])
-      };
 
     }]
   );
