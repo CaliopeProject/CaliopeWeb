f1025db79e816a28a43d91cfb648bfb3654ca809
ndochoa <ndaniel8a@gmail.com>
2013-09-17 17:53:22 -0500

    Add directive to button execute task

--- app/task/task-directives.js
+++ app/task/task-directives.js
@@ -5,10 +5,10 @@
 define(['angular'], function(angular) {
   'use strict';
 
-  angular.module('task-directives',['task-services'])
+  var moduleDirectives = angular.module('task-directives',['task-services'])
   // The loginToolbar directive is a reusable widget that can show login or logout buttons
   // and information the current authenticated user
-  .directive('widgetTask', ['taskService', function(taskService) {
+  moduleDirectives.directive('widgetTask', ['taskService', function(taskService) {
     var directive = {
       templateUrl: 'task/partial-task-widget-task.html',
       restrict: 'E',
@@ -37,4 +37,21 @@ define(['angular'], function(angular) {
 
     return directive;
   }]);
+
+
+  moduleDirectives.directive('cwTaskExecute', ['taskService', function(taskService) {
+    var directive = {
+      templateUrl: 'task/partial-task-execute-template.html',
+      restrict: 'E',
+      replace: true,
+      link: function($scope, $element, $attrs, $controller) {
+
+        console.log('Link cwTaskExecute')
+
+      }
+    };
+
+    return directive;
+  }]);
+
 });
