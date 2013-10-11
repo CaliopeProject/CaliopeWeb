d0a2766ee26ba0e3cb178fa23d41fb1400daf9f4
aurigadl <aurigadl@gmail.com>
2013-10-11 14:58:41 -0500

    summary proyect runing with percentage

--- app/proyectomtv/proyectomtv-summary-controller.js
+++ app/proyectomtv/proyectomtv-summary-controller.js
@@ -7,30 +7,61 @@ define(['angular'], function (angular) {
   dirmodule.controller("summaryProyecto",
     ["$scope",'taskService', function($scope, taskService) {
       $scope.tasks = [];
+      var maxpoint = 10;
+
+      var result = function(category, total){
+        var value = {'Done':maxpoint,'Doing': 0,'ToDo':0};
+        if(!angular.isUndefined(value[category])){
+          total.users++;
+          total.points+= value[category];
+          return total;
+        }
+        return total;
+      };
+
+      var barclass = function(total){
+        if(total > 80){return "-success";}
+        if((60 < total) && (total < 80)){return "";}
+        if((40 < total) && (total < 60)){return "-warning";}
+        if((20 < total) && (total < 40)){return "-info";}
+        return "-danger";
+      };
+
       taskService.getAll().then(function(allTaskValues){
         angular.forEach(allTaskValues,function(vtask){
-          var user, image;
-          var category = [];
-          var name = vtask.name;
+
+          var user, image, bclass;
+          var total = {users: 0, points: 0};
+          var category    = {};
+          var name        = vtask.name;
           var descripcion = vtask.descripcion;
+
           angular.forEach(vtask.holders.target, function(vtarget){
             var user   = vtarget.entity_data;
             var exist  = false;
             angular.forEach(category, function(vcategory, kcategory){
-              if(vcategory.name === vtarget.properties.category){
+              if(vcategory[kcategory] === vtarget.properties.category){
                 exist = true;
-                if(angular.isUndefined(category[kcategory].users)){
-                  category[kcategory].push({users: [user]});
-                }else{
-                  category[kcategory].users.push(user);
-                }
+                category[kcategory].push(user);
+                total = result(kcategory, total);
               }
             });
             if(!exist){
-              category.push({name : vtarget.properties.category, users: [user]});
+              category[vtarget.properties.category] = [];
+              category[vtarget.properties.category].push(user);
+              total = result(vtarget.properties.category, total);
             }
           });
-          $scope.tasks.push({'name': name, desc: descripcion, cate: category});
+
+          total = (total.users === 0)? 0 : (total.points * 100)/( total.users * maxpoint);
+
+          $scope.tasks.push({
+            'name'  : name,
+            'desc'  : descripcion,
+            'cate'  : category,
+            'total' : total,
+            'bclass': barclass(total)
+          });
         });
       });
     }]);
