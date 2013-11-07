94fe229ee92c652d0c982cd49bf265b8f07be1f6
ndochoa <ndaniel8a@gmail.com>
2013-11-07 16:25:20 -0500

    Fix bug in execute task from kanban by change in relations

--- app/tools-kanban-board/kanban-board-controller.js
+++ app/tools-kanban-board/kanban-board-controller.js
@@ -16,7 +16,6 @@ define(['angular', 'angular-dragdrop', 'task-controllers','task-directives'], fu
         });
       });
 
-
       $scope.data = taskService.getTask();
       $scope.showSubtasks = false;
 
@@ -80,6 +79,15 @@ define(['angular', 'angular-dragdrop', 'task-controllers','task-directives'], fu
 
       $scope.addComment   = function (parentTask, text, category){
         taskService.addComment(parentTask, text, category);
+      };
+
+      $scope.getTargetUUID = function(target) {
+        if( target !== undefined ) {
+          var varName = undefined;
+          for( varName in target ) {
+            return varName;
+          }
+        }
       };
 
   }]);
